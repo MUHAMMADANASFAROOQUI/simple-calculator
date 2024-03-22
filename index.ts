8203c0aa2a4ce180d0cded3948272e2a22d6781a
Muhammad Anas <anasshafat7@gmail.com>
2024-03-22 09:36:50 +0500

    push my simple calculator to github

---
+++ index.ts
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  {
+    message: "Enter Your First Number",
+    type: "number",
+    name: "firstNumber",
+  },
+  {
+    message: "Enter Your Second Number",
+    type: "number",
+    name: "secondNumber",
+  },
+  {
+    message: "Seclect one of the Operator",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+
+console.log(answer);
+
+if (answer.operator === "Addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "Subtraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+} else {
+  console.log("Please Enter Valid Operator");
+}
